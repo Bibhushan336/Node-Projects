7cacef4817825614a18d37b23eaeb090ec3335bb
Bibhushan <thapabibhushan@gmail.com>
2022-07-14 13:06:16 +0545

    Routes and Middleware

--- Task Manager/app.js
+++ Task Manager/app.js
@@ -25,9 +25,21 @@ app.use('/api/v1/tasks', tasks)
 // app.delete('/api/v1/tasks/:id')          - delete task
 
 
-const port = 3000
+app.use('/api/v1/tasks', tasks);
 
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5000;
 
-app.listen(3000, () => {
-    console.log('server is listening')
-})
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();

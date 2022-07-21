140924de27e0fce099a5987f2366d74228503075
Bibhushan <thapabibhushan@gmail.com>
2022-07-21 13:05:26 +0545

    API: Create a new user

--- crud_app/server/controller/controller.js
+++ crud_app/server/controller/controller.js
@@ -4,7 +4,31 @@ var Userdb = require('../model/model')
 // create and save new user
 
 exports.create = (req,res) => {
+    // validate request
+    if(!req.body){
+        res.state(400).send({message: "Content can not be empty"})
+        return
+    }
 
+    // new user
+    const user = new Userdb({
+        name : req.body.name,
+        email : req.body.email,
+        gender: req.body.gender,
+        status: req.body.status
+    })
+
+    // save user in the database
+    user
+    .save(user)
+    .then(data => {
+        res.send(data)
+    })
+    .catch(err => {
+        res.status(500).send({
+            message : err.message || "Some error occured while creating a create operation"
+        })
+    })
 }
 
 //retrive and return all users/retrive and return a single user

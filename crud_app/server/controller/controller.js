1ecefe688acf7e095f591f93799197430b2a355b
Bibhushan <thapabibhushan@gmail.com>
2022-07-21 15:40:02 +0545

    API: Retrieving single user from the database

--- crud_app/server/controller/controller.js
+++ crud_app/server/controller/controller.js
@@ -34,13 +34,30 @@ exports.create = (req,res) => {
 //retrive and return all users/retrive and return a single user
 
 exports.find = (req, res) => {
-    Userdb.find()
-    .then(user => {
-        res.send(user)
-    })
-    .catch(err => {
-        res.status(500).send({messgae: err.message || "Error Occured while retriving user information"})
-    })
+    if(req.query.id){
+        const id = req.query.id
+
+        Userdb.findById(id)
+            .then(data => {
+                if(!data){
+                    res.status(404).send({message : "Not found user with id" + id})
+                }else{
+                    res.send(data)
+                }
+            })
+            .catch(err => {
+                res.status(500).send({message: "Error retrieving user with id"})
+            })
+
+    }else{
+        Userdb.find()
+        .then(user => {
+            res.send(user)
+        })
+        .catch(err => {
+            res.status(500).send({messgae: err.message || "Error Occured while retriving user information"})
+        })
+    }
 }
 
 //Update a new identified user by user id

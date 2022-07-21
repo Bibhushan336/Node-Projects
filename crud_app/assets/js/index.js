55dc7fbdf3ea337be683f452f090a607b0dc155b
Bibhushan <thapabibhushan@gmail.com>
2022-07-21 17:22:42 +0545

    Delete and Project Completed

--- crud_app/assets/js/index.js
+++ crud_app/assets/js/index.js
@@ -33,3 +33,23 @@ $('#update_user').submit(function(event){
 })
 
 
+if(window.loactaion.pathname =="/"){
+    $ondelete = $(".table tbody td a.delete")
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request = {
+            "url": `http://localhost:3000/api/users/${id}`,
+            "method": "DELETE"
+            
+        }
+
+        if(confirm("Do you really want to delete this record?")){
+            alert("Data Deleted Successfully")
+            location.reload()
+
+        }
+    })
+}
+
+
